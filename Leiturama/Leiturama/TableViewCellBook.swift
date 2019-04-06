65d471db87e22fb10ff3b36d17c51a81b5a05170
Danillo Guimarães <danillo@danilloguimaraes.com.br>
2019-04-06 15:49:01 -0300

    Criacao da tela de listagem de livros

---
+++ Leiturama/Leiturama/TableViewCellBook.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCellBook.swift
+//  Leiturama
+//
+//  Created by Danillo Oliveira on 06/04/19.
+//  Copyright © 2019 Arthur Santos. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellBook: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Leiturama/Pods/Target Support Files/Pods-Leiturama/Pods-Leiturama-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Leiturama : NSObject
+@end
+@implementation PodsDummy_Pods_Leiturama
+@end
